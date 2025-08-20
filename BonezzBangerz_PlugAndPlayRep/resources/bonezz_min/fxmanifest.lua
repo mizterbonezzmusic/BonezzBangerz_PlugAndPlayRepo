6ad9c982ca269277b464aaeca06f022db7c78fb0
mizterbonezzmusic <mizterbonezzmusic@gmail.com>
2025-08-20 05:41:11 -0700

    Add files via upload

---
+++ BonezzBangerz_PlugAndPlayRep/resources/bonezz_min/fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'mizterbonezzmusic'
+description 'Placeholder manifest for bonezz_min'
+version '1.0.0'
+
+client_scripts {
+    'client/*.lua'
+}
+
+server_scripts {
+    'server/*.lua'
+}
---
+++ BonezzBangerz_PlugAndPlayRep/resources/bonezz_rx_loader/fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'mizterbonezzmusic'
+description 'Placeholder manifest for bonezz_rx_loader'
+version '1.0.0'
+
+client_scripts {
+    'client/*.lua'
+}
+
+server_scripts {
+    'server/*.lua'
+}
